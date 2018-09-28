ead8cfd75820595338812b05f150e8a6ce68fd35
atul <atul.sharma@ofbusiness.in>
2018-09-28 15:24:31 +0530

    exception handling

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 import org.apache.log4j.Logger;
 
+import java.util.AbstractCollection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,13 +25,19 @@ public class Application
             parallelTasks.add(getRunnable("task"+i,logger));
         }
         Runnable end = getRunnable("end",logger);
-        taskExecutor.execute(start,parallelTasks,end);
+        try {
+            taskExecutor.execute(start, parallelTasks, end);
+        }catch (RuntimeException ex){
+            logger.error(ex);
+        }
     }
 
     public static Runnable getRunnable(String name,Logger logger){
         return new Runnable() {
             @Override
             public void run() {
+                if(name.contains("1"))
+                    throw new RuntimeException("Exception in parallel task");
                 logger.info(name);
             }
         };
--- src/main/java/com/example/TaskExecutor.java
+++ src/main/java/com/example/TaskExecutor.java
@@ -9,21 +9,25 @@ public class TaskExecutor {
 
     private Logger logger = Logger.getLogger(TaskExecutor.class);
 
-    public void execute(Runnable start, List<Runnable> paralledTasks,Runnable end){
-        CompletableFuture futureStart = CompletableFuture.runAsync(start);
-        futureStart.join();
-        logger.debug("Finished Task1");
-        List<CompletableFuture<Void>> futures=paralledTasks
-                .stream()
-                .map(s-> CompletableFuture.runAsync(s))
-                .collect(Collectors.toList());
-        CompletableFuture<Void> completableFutures=
-                CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
-        completableFutures.join();
-        logger.debug("Finished parallelTasks");
+    public void execute(Runnable start,final List<Runnable> paralledTasks,Runnable end){
+        try {
+            CompletableFuture futureStart = CompletableFuture.runAsync(start);
+            futureStart.join();
+            logger.debug("Finished Task1");
+            List<CompletableFuture<Void>> futures = paralledTasks
+                    .stream()
+                    .map(s -> CompletableFuture.runAsync(s))
+                    .collect(Collectors.toList());
+            CompletableFuture<Void> completableFutures =
+                    CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
+            completableFutures.join();
+            logger.debug("Finished parallelTasks");
 
-        CompletableFuture futureEnd = CompletableFuture.runAsync(end);
-        futureEnd.join();
-        logger.debug("Finished End Task");
+            CompletableFuture futureEnd = CompletableFuture.runAsync(end);
+            futureEnd.join();
+            logger.debug("Finished End Task");
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
 }
